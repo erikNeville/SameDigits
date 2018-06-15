841ffcc09a38d9ae69d79d78c3de96243f9c02d4
Erik Neville <erikeneville@gmail.com>
2018-06-15 13:30:09 -0700

    solved in a very inefficient way

--- src/NumbersGame/NumbersGame.java
+++ src/NumbersGame/NumbersGame.java
@@ -5,6 +5,8 @@ package NumbersGame;
 
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 /**
  *
@@ -13,33 +15,59 @@ import java.util.LinkedList;
 public class NumbersGame {
 
     static boolean isRepeat(int n) {
-        // ^(\d)\1*$
-        String st = String.valueOf(n);
+        
+        // Create a String to store each number
+        String str;
+        boolean mult = false;
+        
+        // Iterating through the list
+        for (int i = 1; i <= n; i++) {
+            
+            // set str to the value of the integer
+            str = String.valueOf(i);
+            
+            // detect how many digits are in the integer
+            int strlen = str.length();
 
-        for (int i = 0; i < st.length() - 1; i++) {
-            char ch = st.charAt(i);
+            // true or false based on matching digits in different positions
+            switch (strlen) {
+                case 1:
+                    mult = false;
+                    break;
+                case 2:
+                    mult = str.charAt(0) == str.charAt(1);
+                    break;
+                case 3:
+                    mult = str.charAt(0) == str.charAt(1) &&
+                            str.charAt(1) == str.charAt(2) &&
+                            str.charAt(0) == str.charAt(2);
+                    break;
+                case 4:
+                    mult = str.charAt(0) == str.charAt(1) &&
+                            str.charAt(0) == str.charAt(2) &&
+                            str.charAt(0) == str.charAt(3) &&
+                            str.charAt(1) == str.charAt(2) &&
+                            str.charAt(1) == str.charAt(3) &&
+                            str.charAt(2) == str.charAt(3);
+                    break;
+                default:
+                    mult = false;
+            }
         }
-        return false;
+        return mult;
     }
 
     public static void main(String[] args) {
-        int n = 100;
-        int[] a = new int[n];
-        LinkedList<Integer> stack = new LinkedList<>();
-        for (int i = 0; i < a.length; i++) {
-            a[i] = i + 1;
+        int n = 9999;
 
-            while (a[i] > 0) {
-                stack.push(a[i] % 10);
-                a[i] = a[i] / 10;
-            }
-            int[] b = new int[stack.size()];
-            while (!stack.isEmpty()) {
-                System.out.println(stack.pop());
+        for (int i = 1; i <= n; i++) {
+            if (isRepeat(i) == true) {
+                System.out.println(i + " is a repeat");
+            } else {
+                System.out.println(i);
             }
         }
     }
 }
 
-// String.valueOf(i).contains("7")
 
